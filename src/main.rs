67b8e62fe25d758aa545974baf9f5830bf60a23d
codeslayer <willyanpublic@protonmail.com>
2023-01-16 19:46:03 -0300

    rename project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use termion::{color, raw::IntoRawMode};
-use termjumper::{components::Stage, graphics::Sprite, runner::Runner, Game};
+use crabbit::{components::Stage, graphics::Sprite, runner::Runner, Game};
 
 fn main() {
     let mut stdout = io::stdout().into_raw_mode().unwrap();
